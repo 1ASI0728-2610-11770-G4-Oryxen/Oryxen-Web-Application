e74cd8399374dbfc008c3ad586440031059ad8ae
Pedro Nanfuñay <pedro18nan@gmail.com>
2026-06-16 12:45:04 -0500

    initial commit.

---
+++ src/main.js
@@ -0,0 +1,51 @@
+import { createApp } from 'vue'
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
+import App from './App.vue';
+import { Button } from "primevue";
+import InputText from 'primevue/inputtext';
+import Toast from 'primevue/toast';
+import ToastService from 'primevue/toastservice';
+import ConfirmationService from 'primevue/confirmationservice';
+import 'primeicons/primeicons.css'
+import './style.css'
+import router from './router';
+import { createPinia } from 'pinia';
+import './firebase.ts';
+
+// Crear la app
+const app = createApp(App);
+
+// Registrar componentes globales
+app.component('Button', Button);
+app.component('InputText', InputText);
+app.component('Toast', Toast);
+
+// Configurar PrimeVue
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura,
+    },
+});
+
+// Servicios globales
+app.use(ToastService);
+app.use(ConfirmationService);
+
+// Configurar Pinia
+const pinia = createPinia();
+app.use(pinia);
+
+/** Inicializar store de autenticación
+import { useAuthenticationStore } from './iam/services/Authentication.Store';
+const authStore = useAuthenticationStore();
+try {
+    authStore.initialize();
+} catch (e) {
+    console.warn('No se pudo inicializar el store de autenticación', e);
+}**/
+
+// Usar router
+app.use(router);
+
+app.mount('#app')
